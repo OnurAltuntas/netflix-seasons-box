104ffcc8b7c8af139de6eb9f1b2fc38a77accf05
Onur Altuntas <53194850+OnurAltuntas@users.noreply.github.com>
2020-10-16 20:52:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Data from "./Data";
 
 function App() {
+  const [data, setdata] = useState(Data);
+  const [name, setname] = useState("Black Mirror");
+  const [whichSeason, setwhichSeason] = useState(3);
+  const [selectedSeason, setselectedSeason] = useState("");
+
+  const nextClick = (item_id, item_name) => {
+    var table = document.getElementsByClassName("table")[0];
+    table.style.display = "block";
+    setselectedSeason(item_id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="canvas">
+      <div className="bg">
+        {selectedSeason ? (
+          <h2
+            className="season"
+            onClick={() => {
+              setselectedSeason("");
+            }}
+          >
+            {" "}
+            🡠 {selectedSeason}. Season
+          </h2>
+        ) : (
+          <h2 id="name"> {name}</h2>
+        )}
+        <div className="table">
+          <div className="column">
+            {selectedSeason
+              ? data.seasons.map((item) => (
+                  <div className="line">
+                    {item.episodes
+                      .filter((x) => x.id == whichSeason)
+                      .map((y) => (
+                        <div className="column">
+                          <h2 className="season">{y.name}</h2>
+
+                          <h2 onClick={() => {}} className="arrow">
+                            <img
+                              className="thumbnail"
+                              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpOOxI0BQyfO5VQJVZdZxujd0oGd_DiHuKAw&usqp=CAU"
+                              alt="thumnail"
+                              with="100"
+                              height="80"
+                            ></img>
+                            <p className="summary">
+                              Incididunt sunt nostrud nisi ad ea ut tempor in.
+                              Eu veniam consequat duis velit aliqua. .
+                            </p>
+                          </h2>
+                        </div>
+                      ))}
+                  </div>
+                ))
+              : data.seasons.map((item) => (
+                  <div className="line">
+                    {whichSeason == item.id ? (
+                      <h2 className="check">✔</h2>
+                    ) : (
+                      <h2 className="check"></h2>
+                    )}
+                    <h2 className="season"> {item.id}. Season</h2>
+                    <h2
+                      onClick={() => {
+                        nextClick(item.id, item.name);
+                      }}
+                      className="arrow"
+                    >
+                      🡢
+                    </h2>
+                  </div>
+                ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,78 @@
+const data = {
+    "seasons":[
+      {
+        "id":1,
+        "name":1,
+        "episodes":[
+          {
+            "id":"1",
+            "name":"USS Calister"
+          },
+          {
+            "id":"2",
+            "name":"USS Calister"
+          },
+          {
+            "id":"3",
+            "name":"USS Calister"
+          },
+        ]
+      },
+      {
+        "id":2,
+        "name":1,
+        "episodes":[
+          {
+            "id":"1",
+            "name":"USS Calister"
+          },
+          {
+            "id":"2",
+            "name":"USS Calister"
+          },
+          {
+            "id":"3",
+            "name":"USS Calister"
+          },
+        ]
+      },
+      {
+        "id":3,
+        "name":1,
+        "episodes":[
+          {
+            "id":"1",
+            "name":"National "
+          },
+          {
+            "id":"2",
+            "name":"USS Calister"
+          },
+          {
+            "id":"3",
+            "name":"USS Calister"
+          },
+        ]
+      },
+      {
+        "id":4,
+        "name":1,
+        "episodes":[
+          {
+            "id":"1",
+            "name":"USS Calister"
+          },
+          {
+            "id":"2",
+            "name":"USS Calister"
+          },
+          {
+            "id":"3",
+            "name":"USS Calister"
+          },
+        ]
+      }
+    ]
+  }
+
+  export default data;
